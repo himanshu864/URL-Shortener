5b3df88718c636db24d12b6c3b3a9130dfca5d9c
himanshu864 <himanshu96864@gmail.com>
2024-06-28 00:30:49 +0530

    minor: vercel deploy pls:

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000);
+/*
+const urlRouter = require("./routers/url.js");
+const idRouter = require("./routers/id.js");
+const staticRouter = require("./routers/static.js");
+const userRouter = require("./routers/user.js");
+const adminRouter = require("./routers/admin.js");
+
+const cookieParser = require("cookie-parser");
+const connectMongoDB = require("./config/connect.js");
+connectMongoDB("mongodb://127.0.0.1:27017/short-url-auth");
+
+const {
+  restrictUnauthenticated,
+  restrictUnauthorized,
+} = require("./middlewares/unauth.js");
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/URL", restrictUnauthenticated, urlRouter); // Generate ShortURL
+app.use("/r", idRouter); // RirectURL
+app.use("/user", userRouter); // User Authentication
+app.use("/admin", restrictUnauthenticated, restrictUnauthorized, adminRouter); // Admin Access
+app.use("/", staticRouter); // Server Static Files
+
+app.use((err, req, res, next) => res.status(500).json({ error: err.message }));
+*/
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
